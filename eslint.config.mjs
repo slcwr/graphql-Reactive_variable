aae464504d796b8be803f3d74e92e0e075a32576
a.y <g.slackware@gmail.com>
2024-11-27 13:36:49 +0000

    test10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,17 @@
-import js from '@eslint/js';
-import tsParser from '@typescript-eslint/parser';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
 export default [
+  ...compat.config({
+    extends: ['next/core-web-vitals']
+  }),
   js.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
@@ -26,8 +36,6 @@ export default [
   },
   {
     ignores: ['node_modules/**', '.next/**', 'backend/src/generated/**','**/*.d.ts','backend/dist/**','frontend/cypress/**' ]
-  },
-  ...compat.config({
-    extends: ['next/core-web-vitals']
-  })
+  }
+  
 ];
